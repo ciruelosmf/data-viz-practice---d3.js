c44faa8fe3c17653bd117040a3687fc81b757936
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-05-23 00:24:45 +0000

    js change text of btn dnst work

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
+
+
+
+
+
 // targeting the svg itself
 const svg = document.getElementById("svs");
 
 // variable for the namespace 
 const svgns = "http://www.w3.org/2000/svg";
 
+let newRect = document.createElementNS(svgns, "rect");
 // make a simple rectangle
 let newRect = document.createElementNS(svgns, "rect");
 let a = 60;
@@ -16,7 +23,46 @@ newRect.setAttribute("fill", "#5cceee");
 // append the new rectangle to the svg
 svg.appendChild(newRect);
 
+
+
+
+
+
+
+const  boto = document.getElementById("hola");
+
+
+
+boto.addEventListener("click", ()=>{
+
+
+        boto.innerText= "oh no";
+    
+});
+
+
+
+
+
+
+
  function showS() {
    newRect.setAttribute("y", a);
    a += 60;
  }
+
+
+
+
+
+/* 
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    d3.select("body").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .attr("class", "bar").style("height", d => `${d*5}px`)
+      // Add your code below this line
+
+*/
